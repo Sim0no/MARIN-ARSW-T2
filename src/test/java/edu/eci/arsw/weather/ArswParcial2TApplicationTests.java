2b5226a66987fa5e707a67abd8334aaffbb44e3a
Simón Marín <german.marin@mail.escuelaing.edu.co>
2021-04-07 06:40:15 -0500

    creando

---
+++ src/test/java/edu/eci/arsw/weather/ArswParcial2TApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ARSWPARCIAL2T;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArswParcial2TApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
