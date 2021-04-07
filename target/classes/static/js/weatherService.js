bcd3cf770b8e102c4c030a55c869edf818f4c2a6
Simón Marín <german.marin@mail.escuelaing.edu.co>
2021-04-07 09:40:03 -0500

    heroku

--- target/classes/static/js/weatherService.js
+++ target/classes/static/js/weatherService.js
@@ -3,7 +3,7 @@ weather = (function (){
 
     return{
         getWeatherStatsByCity: function(city, callback){
-            var getStat = $.getJSON("/api//weather/"+city, function(){
+            var getStat = $.getJSON("/api/weather/"+city, function(){
                 let response = getStat.responseText;
             }).done(()=>{
                 callback(JSON.parse(getStat.responseText));
